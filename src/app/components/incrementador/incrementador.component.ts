e348aaa0faee6d458e2f021fa81f234b258cec14
RoberRM <roberto.ruiz.martin@gmail.com>
2022-04-04 20:20:29 +0200

    Clase 54. Graficas

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -6,21 +6,37 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class IncrementadorComponent {
-
-  progreso: number = 50;
-  get getPorcentaje() {
-    return `${this.progreso}%`;
+export class IncrementadorComponent implements OnInit{
+  ngOnInit(): void {
+    this.btnClass = `btn ${this.btnClass}`;
   }
+
+  @Input('valor') progreso: number = 40;
+  @Input() btnClass: string = 'btn btn-primary';
+
+  @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter();
 
   cambiarValor(valor: number) {
     if (this.progreso >= 100 && valor >= 0) {
+      this.valorSalida.emit(100);
       return this.progreso = 100;
     }
     if (this.progreso <= 0 && valor < 0) {
+      this.valorSalida.emit(0);
       return this.progreso = 0;
     }
     this.progreso = this.progreso + valor;
+    this.valorSalida.emit(this.progreso);
+  }
+
+  onChange(nuevoValor: number) {
+    if (nuevoValor >= 100) {
+      this.progreso = 100;
+    } else if (nuevoValor <=0) {
+      this.progreso = 0;
+    } else {
+      this.progreso = nuevoValor;
+    }
   }
 
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
+import { NgChartsModule } from 'ng2-charts';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
@@ -31,7 +33,8 @@ import { ComponentsModule } from '../components/components.module';
     SharedModule,
     RouterModule,
     FormsModule,
-    ComponentsModule
+    ComponentsModule,
+    NgChartsModule
   ]
 })
 export class PagesModule { }
